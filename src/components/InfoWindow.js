2e55a82c1ccfaa2280798ba113e0ed8aaf4b8511
Laura Logan <laurardh@aol.com>
2018-09-03 13:12:48 -0400

    adds live link to readme

---
+++ src/components/InfoWindow.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import  locations  from '../data/locations.js';
+import PlacesList from './PlacesList.js';
+
+class App extends Component {
+  render() {
+    return(
+
+    );
+  }
+}
+
+export default InfoWindow;
