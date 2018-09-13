43ef3f576573f8455a723b469731a5c35f445ac8
Laura Logan <laurardh@aol.com>
2018-09-13 14:09:00 -0400

    edits build for infoWindow

--- src/components/App.js
+++ src/components/App.js
@@ -18,6 +18,7 @@ class App extends Component {
       showFiltered: true,
     }
     this.toggleList = this.toggleList.bind(this);
+    // this.populateInfoWindow = this.populateInfoWindow.bind(this);
   }
 
   toggleList = () => {
@@ -39,81 +40,9 @@ class App extends Component {
       }
     });
   }
-
-  // populateInfoWindow = (marker, infowindow, map) => {
-  // 	//check to see if infowindow is already open
-  // 	if (infowindow.marker != marker) {
-  // 		//clear the infowindow content allowing streetview to load
-  // 		infowindow.setContent('');
-  // 		infowindow.marker = marker;
-  // 		this.getPlacesDetails(marker, infowindow);
-  // 		//see if the marker property is cleared if infowindow is closed
-  // 		infowindow.addListener('closeclick', function() {
-  // 			// vm.showMe(true);
-  // 			// vm.showFS(true);
-  // 			infowindow.marker = null;
-  // 			// marker.setIcon(defaultIcon);
-  // 			// vm.fourSqFinds([]);
-  // 		});
-  // 		//open the infowindow on the proper marker
-  // 		infowindow.open(map, marker);
-  // 		marker.setAnimation(window.google.maps.Animation.BOUNCE);
-  // 		setTimeout(function() {
-  // 			marker.setAnimation(null);
-  // 		}, 5000);
-  // 	}
-  // }
-  //
-  // //gets place details from place_id via PlacesService
-  //  getPlacesDetails = (marker, infowindow, map) => {
-  // 	let service = new window.google.maps.places.PlacesService(map);
-  // 	service.getDetails({
-  // 		placeId: marker.id
-  // 	}, function(place, status) {
-  // 		if (status === window.google.maps.places.PlacesServiceStatus.OK) {
-  // 			// Set the marker property on this infowindow so it isn't created again.
-  // 			infowindow.marker = marker;
-  // 			var innerHTML = '<div id = "places-details">';
-  // 			innerHTML += ' <div id = "place-name">';
-  // 			if (place.name) {
-  // 				innerHTML += '<strong>' + place.name + '</strong>';
-  // 			}
-  // 			innerHTML += '</div>';
-  // 			if (place.formatted_address) {
-  // 				innerHTML += '<br>' + place.formatted_address;
-  // 			}
-  // 			if (place.formatted_phone_number) {
-  // 				innerHTML += '<br>' + place.formatted_phone_number;
-  // 			}
-  // 			if (place.opening_hours) {
-  // 				innerHTML += '<br><br><strong>Hours:</strong><br>' + place.opening_hours.weekday_text[0] + '<br>' + place.opening_hours.weekday_text[1] + '<br>' + place.opening_hours.weekday_text[2] + '<br>' + place.opening_hours.weekday_text[3] + '<br>' + place.opening_hours.weekday_text[4] + '<br>' + place.opening_hours.weekday_text[5] + '<br>' + place.opening_hours.weekday_text[6];
-  // 			}
-  // 			if (place.website) {
-  // 				innerHTML += '<br><br><a id = "website" href=' + place.website + '>' + place.website + '</a>';
-  // 			}
-  // 			if (place.photos) {
-  // 				innerHTML += '<br><br><img src="' + place.photos[0].getUrl({
-  // 					maxHeight: 100,
-  // 					maxWidth: 200
-  // 				}) + '">';
-  // 			}
-  // 			//creates #pano element for streetViewService to use
-  // 			innerHTML += ' <div id = "pano"></div>';
-  // 			innerHTML += '<div class="iw-bottom-gradient"></div>';
-  // 			innerHTML += '</div>';
-  // 			infowindow.setContent(innerHTML);
-  // 			infowindow.open(map, marker);
-  // 			// Make sure the marker property is cleared if the infowindow is closed.
-  // 			infowindow.addListener('closeclick', function() {
-  // 				infowindow.marker = null;
-  // 			});
-  // 		}
-  // 	});
-  // }
-
-
-
+  
   componentWillReceiveProps({ isScriptLoaded, isScriptLoadSucceed }) {
+    const { marker, infowindow, map } = this.state;
     // const { isScriptLoaded, isScriptLoadSucceed } = this.props;
     if (isScriptLoaded && !this.props.isScriptLoaded) { // load finished
       if (isScriptLoadSucceed) {
@@ -151,16 +80,16 @@ class App extends Component {
                   marker.setAnimation(null);
                 }, 2500);
 
-                this.populateInfoWindow(this.marker, this.infowindow,this.map);
+              populateInfoWindow(marker, infowindow, map);
 
               //get locations information
 
               //create infowindow
-              marker.infowindow = `div class="infowindow">
-                                    <div class="item-info">
-                                      <h3 class="item-name">
-                                    </div>
-                                  </div>`
+              // marker.infowindow = `div class="infowindow">
+              //                       <div class="item-info">
+              //                         <h3 class="item-name">
+              //                       </div>
+              //                     </div>`
 
               });
             });
--- src/data/placesDetails.js
+++ src/data/placesDetails.js
@@ -33,42 +33,32 @@ export const getPlacesDetails = (marker, infowindow, map) => {
 		if (status === window.google.maps.places.PlacesServiceStatus.OK) {
 			// Set the marker property on this infowindow so it isn't created again.
 			infowindow.marker = marker;
-			var innerHTML = '<div id = "places-details">';
-			innerHTML += ' <div id = "place-name">';
-			if (place.name) {
-				innerHTML += '<strong>' + place.name + '</strong>';
-			}
-			innerHTML += '</div>';
-			if (place.formatted_address) {
-				innerHTML += '<br>' + place.formatted_address;
-			}
-			if (place.formatted_phone_number) {
-				innerHTML += '<br>' + place.formatted_phone_number;
-			}
-			if (place.opening_hours) {
-				innerHTML += '<br><br><strong>Hours:</strong><br>' + place.opening_hours.weekday_text[0] + '<br>' + place.opening_hours.weekday_text[1] + '<br>' + place.opening_hours.weekday_text[2] + '<br>' + place.opening_hours.weekday_text[3] + '<br>' + place.opening_hours.weekday_text[4] + '<br>' + place.opening_hours.weekday_text[5] + '<br>' + place.opening_hours.weekday_text[6];
-			}
-			if (place.website) {
-				innerHTML += '<br><br><a id = "website" href=' + place.website + '>' + place.website + '</a>';
-			}
-			if (place.photos) {
-				innerHTML += '<br><br><img src="' + place.photos[0].getUrl({
-					maxHeight: 100,
-					maxWidth: 200
-				}) + '">';
-			}
+			marker.content = `<div id = "places-details">
+													<h3 class="place-name">${place.name}</h3>
+				 									<p class="place-address">${place.formatted_address}</p>
+													<p class="place-phone">${place.formatted_phone_number}</p>
+													<div class="place-hours">
+														${place.opening_hours.weekday_text[0]}
+														${place.opening_hours.weekday_text[1]}
+														${place.opening_hours.weekday_text[2]}
+														${place.opening_hours.weekday_text[3]}
+														${place.opening_hours.weekday_text[4]}
+														${place.opening_hours.weekday_text[5]}
+														${place.opening_hours.weekday_text[6]}
+													</div>
+													<a id = "website" href=${place.website}>${place.website}</a>
+													<img class= "place-img" src=${place.photos[0]}>
+											 </div>`
 			//creates #pano element for streetViewService to use
-			innerHTML += ' <div id = "pano"></div>';
-			innerHTML += '<div class="iw-bottom-gradient"></div>';
-			innerHTML += '</div>';
-			infowindow.setContent(innerHTML);
-			infowindow.open(map, marker);
-			// Make sure the marker property is cleared if the infowindow is closed.
-			infowindow.addListener('closeclick', function() {
-				infowindow.marker = null;
-			});
+			// innerHTML += ' <div id = "pano"></div>';
+			// innerHTML += '<div class="iw-bottom-gradient"></div>';
+			// innerHTML += '</div>';
+			// infowindow.setContent(innerHTML);
+			// infowindow.open(map, marker);
+			// // Make sure the marker property is cleared if the infowindow is closed.
+			// infowindow.addListener('closeclick', function() {
+			// 	infowindow.marker = null;
+			// });
 		}
 	});
 }
-
-// export default placesDetails;
